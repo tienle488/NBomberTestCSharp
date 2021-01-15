1241bc09b2e178855d004e5f5ac9c0a5c20455a2
Tien Le <tile2@cisco.com>
2021-01-14 23:10:09 -0800

    initial commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using NBomber.Contracts;
+using NBomber.CSharp;
+
+namespace NBomberTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            // first, you need to create a step
+            var step = Step.Create("step", async context =>
+            {
+                // you can define and execute any logic here,
+                // for example: send http request, SQL query etc
+                // NBomber will measure how much time it takes to execute your logic
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+                return Response.Ok();
+            });
+            
+            // second, we add our step to the scenario
+            var scenario = ScenarioBuilder.CreateScenario("hello_world", step);
+
+            NBomberRunner
+                .RegisterScenarios(scenario)
+                .Run();
+        }
+    }
+}
